dc1647a0b69392bacf31f144188e6a07e4a55998
anastasiiahulko <148503332+anastasiiahulko@users.noreply.github.com>
2024-09-17 19:21:13 +0200

    Add files via upload

---
+++ Rock-paper-scissors game/ENG_Rock-paper-scissors game/game_with_computer_eng.rb
@@ -0,0 +1,82 @@
+def normalize_choice(choice)
+    if ["rock", "roc", "ro", "r"].include?(choice)
+        "rock"
+    elsif ["paper", "pape", "pap", "pa", "p"].include?(choice)
+        "paper"
+    elsif ["scissors", "scissor", "scisso", "sciss", "scis", "sci", "sci", "sc", "s"].include?(choice)
+        "scissors"
+    else
+        choice
+    end
+end
+
+def computer_play
+    ["rock", "scissors", "paper"].sample
+end
+
+def end_game(choice, user_name, points)
+    if choice == "0"
+        if points.length<=0
+            puts("Game over")
+            exit
+        else
+            puts("Game over")
+            puts("Final score: \n ")
+            puts(point_results(user_name, points))
+            exit
+        
+        end
+    end
+end
+
+
+def point_results(user_name, points)
+    puts(" #{user_name}: #{points[:player]}\n Computer: #{points[:computer]}")
+end
+
+
+def determination_of_the_winner(normal_player_choise, computer_choise, points)
+    
+    if normal_player_choise == computer_choise
+        puts("A draw!")
+    elsif (normal_player_choise == "rock" && computer_choise == "scissors") || 
+        (normal_player_choise == "scissors" &&  computer_choise == "paper") || 
+        (normal_player_choise =="paper" && computer_choise == "rock")
+        puts("You won!")
+        points[:player] +=1
+        
+    else
+        puts("The computer won!")
+        points[:computer] +=1
+    end
+end
+
+puts("Enter your name. Enter 0 to exit the game:")
+player_name = gets.chomp
+
+end_game(player_name, "", "")
+
+points = {player: 0, computer: 0}
+
+
+loop do
+    puts("Choose 'rock', 'scissors' or 'paper'. Enter 0 to exit the game:")
+    player_choise = gets.chomp.downcase
+    
+    end_game(player_choise, player_name, points)
+    
+    normal_player_choise = normalize_choice(player_choise)
+
+    if  ["rock", "scissors", "paper"].include?(normal_player_choise)
+
+        computer_choise = computer_play
+        puts("Computer selection: #{computer_choise}")
+        
+        puts(determination_of_the_winner(normal_player_choise, computer_choise, points))
+        
+        puts("Score:\n")
+        puts(point_results(player_name, points))
+    else
+        puts("Incorrect choice. Try again.")
+    end
+end
---
+++ Rock-paper-scissors game/ENG_Rock-paper-scissors game/game_with_players_eng.rb
@@ -0,0 +1,87 @@
+def end_game(choice, name_of_first_user, name_of_second_user, points)
+    if choice == "0"
+        if points.length<=0
+            puts("Game over")
+            exit
+        else
+            puts("Game over")
+            puts("Final score: \n ")
+            puts(point_results(name_of_first_user, name_of_second_user, points))
+            exit
+        
+        end
+    end
+end
+
+def normalize_choice(choice)
+    if ["rock", "roc", "ro", "r"].include?(choice)
+        "rock"
+    elsif ["paper", "pape", "pap", "pa", "p"].include?(choice)
+        "paper"
+    elsif ["scissors", "scissor", "scisso", "sciss", "scis", "sci", "sci", "sc", "s"].include?(choice)
+        "scissors"
+    else
+        choice
+    end
+end
+
+def point_results(name_of_first_user, name_of_second_user, points)
+    puts(" #{name_of_first_user}: #{points[:player_one]}\n #{name_of_second_user}: #{points[:player_two]}")
+end
+
+def determination_of_the_winner(notmal_first_user_choise, notmal_second_user_choise, name_of_first_user, name_of_second_user, points)
+    
+    if notmal_first_user_choise == notmal_second_user_choise
+        puts("Нічия!")
+    elsif (notmal_first_user_choise == "rock" && notmal_second_user_choise == "scissors") || 
+        (notmal_first_user_choise == "scissors" &&  notmal_second_user_choise == "paper") || 
+        (notmal_first_user_choise == "paper" && notmal_second_user_choise == "rock")
+        puts("#{name_of_first_user} won!")
+        points[:player_one] +=1
+        
+    else
+        puts("#{name_of_second_user} won!")
+        points[:player_two] +=1
+    end
+end
+
+puts("Enter your names. Enter 0 to exit the game:")
+puts("Player 1:")
+name_of_first_user = gets.chomp
+
+end_game(name_of_first_user, "", "", "")
+
+puts("Player 2:")
+name_of_second_user = gets.chomp
+
+end_game(name_of_second_user, "", "", "")
+
+points = {player_one: 0, player_two: 0}
+
+# Камінь ламає ножиці
+# Ножиці ріжуть папір
+# Папір накриває камінь
+
+loop do
+    puts("Player #{name_of_first_user}, choose 'rock', 'scissors' or 'paper'. Enter 0 to exit the game:")
+    first_user_choise = gets.chomp
+    
+    end_game(first_user_choise, name_of_first_user, name_of_second_user, points)
+    
+    notmal_first_user_choise = normalize_choice(first_user_choise)
+
+    puts("Player #{name_of_second_user}, choose 'rock', 'scissors' or 'paper'. Enter 0 to exit the game:")
+    second_user_choise = gets.chomp
+    
+    end_game(second_user_choise, name_of_first_user, name_of_second_user, points)
+    
+    notmal_second_user_choise = normalize_choice(second_user_choise)
+
+    if ["rock", "scissors", "paper"].include?(notmal_first_user_choise) && ["rock", "scissors", "paper"].include?(notmal_second_user_choise)
+        puts(determination_of_the_winner(notmal_first_user_choise, notmal_second_user_choise, name_of_first_user, name_of_second_user, points))
+        puts("Score: \n")
+        puts(point_results(name_of_first_user, name_of_second_user, points))
+    else
+        puts("Incorrect choice. Try again.")
+    end
+end
