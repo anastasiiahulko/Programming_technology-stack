905655900f3adcbbccfa37ebf4cfee0e0271e2eb
anastasiiahulko <148503332+anastasiiahulko@users.noreply.github.com>
2024-09-18 20:50:43 +0200

    Update game_with_computer_eng.rb

--- Rock-paper-scissors game/ENG_Rock-paper-scissors game/game_with_computer_eng.rb
+++ Rock-paper-scissors game/ENG_Rock-paper-scissors game/game_with_computer_eng.rb
@@ -14,15 +14,16 @@ def computer_play
     ["rock", "scissors", "paper"].sample
 end
 
-def end_game(choice, user_name, points)
+def end_game(choice, user_name, points, num_game)
     if choice == "0"
-        if points.length<=0
-            puts("Game over")
-            exit
-        else
+        if num_game > 0
             puts("Game over")
             puts("Final score: \n ")
             puts(point_results(user_name, points))
+            exit
+        else
+            puts("Game over")
+
             exit
         
         end
@@ -54,23 +55,26 @@ def determination_of_the_winner(normal_player_choise, computer_choise, points)
 end
   
 
+num_game = 0
+
 puts("Enter your name. Enter 0 to exit the game:")
 player_name = gets.chomp
 
-end_game(player_name, "", "")
+end_game(player_name, "", "", num_game)
 
 points = {player: 0, computer: 0}
-
 
 loop do
     puts("Choose 'rock', 'scissors' or 'paper'. Enter 0 to exit the game:")
     player_choise = gets.chomp.downcase
     
-    end_game(player_choise, player_name, points)
-    
+    end_game(player_choise, player_name, points, num_game)
+
     normal_player_choise = normalize_choice(player_choise)
 
     if  ["rock", "scissors", "paper"].include?(normal_player_choise)
+
+        num_game+=1
 
         computer_choise = computer_play
         puts("Computer selection: #{computer_choise}")
