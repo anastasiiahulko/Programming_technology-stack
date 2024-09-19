35ee66cb5168fba6da84faadf0254d020ed92cc8
anastasiiahulko <148503332+anastasiiahulko@users.noreply.github.com>
2024-09-19 10:17:10 +0200

    Update game_with_computer_eng.rb

--- Rock-paper-scissors game/ENG_Rock-paper-scissors game/game_with_computer_eng.rb
+++ Rock-paper-scissors game/ENG_Rock-paper-scissors game/game_with_computer_eng.rb
@@ -1,9 +1,10 @@
 def normalize_choice(choice)
-    if ["rock", "roc", "ro", "r"].include?(choice)
+    #нормалізуємо вибір користувачів. коли користувач вводить непоне або часткові слова rock, paper або scissors
+    if ["rock", "roc", "ro", "r", "rok", "rokc", "rck"].include?(choice)
         "rock"
-    elsif ["paper", "pape", "pap", "pa", "p"].include?(choice)
+    elsif ["paper", "pape", "pap", "pa", "p", "papper", "paperr", "papre", "paperr", "papre"].include?(choice)
         "paper"
-    elsif ["scissors", "scissor", "scisso", "sciss", "scis", "sci", "sci", "sc", "s"].include?(choice)
+    elsif ["scissors", "scissor", "scisso", "sciss", "scis", "sci", "sci", "sc", "s", "scisor", "scissoors", "scissorz", "scissoes", "scissoz"].include?(choice)
         "scissors"
     else
         choice
@@ -11,79 +12,92 @@ def normalize_choice(choice)
 end
 
 def computer_play
+    #гернерація вибору комп'ютера
     ["rock", "scissors", "paper"].sample
 end
 
-def end_game(choice, user_name, points, num_game)
+def end_game(choice, player_name, points)
+    #закінчення гри, якщо користувач ввів 0
     if choice == "0"
-        if num_game > 0
+        if points.length != 0 #якщо раунди гри були та ігроки мають бали за гру
             puts("Game over")
             puts("Final score: \n ")
-            puts(point_results(user_name, points))
+            puts(point_results(player_name, points))
             exit
-        else
+        else #якщо ще не було жодного раунду та балів у ігнроків немає
             puts("Game over")
-
             exit
-        
         end
     end
 end
 
 
-def point_results(user_name, points)
-    puts(" #{user_name}: #{points[:player]}\n Computer: #{points[:computer]}")
+def point_results(player_name, points)
+    #виведення результатів гри
+    puts(" #{player_name}: #{points["player"]}\n Computer: #{points["computer"]}")
 end
 
 
-def determination_of_the_winner(normal_player_choise, computer_choise, points)
-    winning_combinations = {
+def determination_of_the_winner(normalized_pleyer_choise, computer_choise, points)
+    #визначення переможців гри 
+    winning_combinations = { #зазначення виграшних комбінацій
       "rock" => "scissors",
       "scissors" => "paper",
       "paper" => "rock"
     }
   
-    if normal_player_choise == computer_choise
+    if normalized_pleyer_choise == computer_choise #якщо вибір користувача та комп'ютера співпадать - нічия
       puts("A draw!")
-    elsif winning_combinations[normal_player_choise] == computer_choise
-      puts("You won!")
-      points[:player] += 1
-    else
-      puts("The computer won!")
-      points[:computer] += 1
+    elsif winning_combinations[normalized_pleyer_choise] == computer_choise #якщо вибір користувача виграшний
+        puts("You won!")
+        if points.key?(:player)==false && points.key?(:computer)==false #якщо це перший раунд та гех балів порожній
+            points["player"] = 1
+            points["computer"] =0
+        else #якщо це не перший раунд та є бали за гру, додаємо 1 бал користувачу
+            points[:player] += 1
+        end
+    else #якщо вибір комп'ютера виграшни
+        puts("The computer won!")
+        if points.key?(:player)==false && points.key?(:computer)==false #якщо це перший раунд та гех балів порожній
+            points["player"] = 0
+            points["computer"] =1
+        else #якщо це не перший раунд та є бали за гру, додаємо 1 бал компйютеру
+            points[:computer] += 1
+        end
     end
 end
-  
 
-num_game = 0
+#тіло гри
 
+#користувач вводить своє їм'я
 puts("Enter your name. Enter 0 to exit the game:")
 player_name = gets.chomp
 
-end_game(player_name, "", "", num_game)
+#перевірка чи користувач ввів 0
+end_game(player_name, "", "")
 
-points = {player: 0, computer: 0}
+#створення пустого гешу для подальшого зберігання балів гравців
+points = Hash.new
 
+#цикл гри. 
 loop do
+    #користувач вводить rock, paper або scissors 
     puts("Choose 'rock', 'scissors' or 'paper'. Enter 0 to exit the game:")
     player_choise = gets.chomp.downcase
     
-    end_game(player_choise, player_name, points, num_game)
+    #перевірка чи користувач ввів 0
+    end_game(player_choise, player_name, points)
 
-    normal_player_choise = normalize_choice(player_choise)
+    #нормалізація вибору користувача, якщо він ввів, наприклад, не повне слово
+    normalized_pleyer_choise = normalize_choice(player_choise )
 
-    if  ["rock", "scissors", "paper"].include?(normal_player_choise)
-
-        num_game+=1
-
-        computer_choise = computer_play
-        puts("Computer selection: #{computer_choise}")
-        
-        puts(determination_of_the_winner(normal_player_choise, computer_choise, points))
-        
-        puts("Score:\n")
-        puts(point_results(player_name, points))
-    else
+    if  ["rock", "scissors", "paper"].include?(normalized_pleyer_choise) #преевірка яи користувач ввів саме rock, paper або scissors 
+        computer_choise = computer_play #призначення вибора комп'ютера
+        puts("Computer selection: #{computer_choise}") #виведення для користувача вибір комп'ютера
+        puts(determination_of_the_winner(normalized_pleyer_choise, computer_choise, points)) #виклик функції визначення переможця раунда
+        puts("Score:\n") #вивід поточного рахунку
+        puts(point_results(player_name, points)) #виклик функції виведення рахунку гри
+    else #вивід повідомлення про некоректний вибір, якщо користувач ввів щось окрім rock, paper або scissors  та кормалізувати його не вийшло
         puts("Incorrect choice. Try again.")
     end
 end
